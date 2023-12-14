afcf978fe11629cd8758b3ac0c9e2b91273e32b0
Daniele Rolli <danielerolli@proton.me>
2023-12-14 22:10:54 +0100

    🏷️ Fixed Tags & writing permissions
    During development, an issue was found in adding multiple tags, where only one of two or more tags actually gets added to the note. On top of that, due to a reset of both the iOS and Android folders, the writing permissions had been removed; they are now back to normal.

---
+++ dist/assets/web-49IWj94g.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-EeXpTbhC.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- dist/assets/web-57FBIn8q.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-Yfav923g.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
