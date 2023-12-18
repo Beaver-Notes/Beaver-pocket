f8c44ffc073ddf8e8a9c2680229405c7fdefbc7b
Daniele Rolli <danielerolli@proton.me>
2023-12-18 09:28:36 +0100

    🚀 Sidebar, TailwindCSS, Darkmode Toggle
    This commit introduces the following:
    
    1. Setting page with dark mode toggle
    2. Sidebar
    3. Switch the Editor, Bottom navbar and sidebar to tailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Home from "./Home";
 import Archive from "./Archive";
+import Settings from "./Settings";
 
 const App: React.FC = () => (
   <Routes>
     <Route path="/" element={<Home />} />
     <Route path="/archive" element={<Archive />} />
+    <Route path="/settings" element={<Settings />}/>
   </Routes>
 );
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
