af87d04cf476a7dbfa816abbab8d177c4a6f397d
Daniele Rolli <danielerolli@proton.me>
2023-12-30 12:23:59 +0100

    🎯 Focus Mode & Lists

--- dist/assets/web-O_UMjtTU.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-9gZ9LaRR.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ dist/assets/web-mKAaZE8a.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-Cqv3vA4s.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
