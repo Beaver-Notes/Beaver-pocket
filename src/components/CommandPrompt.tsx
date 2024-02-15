f543a877c8eb0c3e95d323f918dbf476c146761d
Daniele Rolli <danielerolli@proton.me>
2024-02-15 12:17:10 +0100

    🚚 Moving around some stuff

--- src/components/CommandPrompt.tsx
+++ src/components/CommandPrompt.tsx
@@ -1,88 +1,151 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Note } from "../store/types";
 import Search2LineIcon from "remixicon-react/Search2LineIcon";
+import { useNavigate } from "react-router-dom";
 
-interface ShortcutPromptProps {
+interface CommandPromptProps {
   isOpen: boolean;
   notes: Note[];
   onClickNote: (note: Note) => Promise<void>;
-  setIsShortcutPromptOpen: (value: boolean) => void;
+  setIsCommandPromptOpen: (value: boolean) => void;
+  toggleTheme: () => void;
+  onCreateNewNote: () => void;
 }
 
-const ShortcutPrompt: React.FC<ShortcutPromptProps> = ({
+const CommandPrompt: React.FC<CommandPromptProps> = ({
   isOpen,
   notes,
   onClickNote,
-  setIsShortcutPromptOpen,
+  toggleTheme,
+  setIsCommandPromptOpen,
+  onCreateNewNote,
 }) => {
   const [searchTerm, setSearchTerm] = useState("");
   const [showSubMenu, setShowSubMenu] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
-  // Function to handle key press events
+  const filteredNotes = notes.filter((note) =>
+    note.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (isOpen) {
+        if (e.key === "ArrowUp") {
+          setSelectedIndex((prevIndex) => (prevIndex > 0 ? prevIndex - 1 : 0));
+        } else if (e.key === "ArrowDown") {
+          setSelectedIndex((prevIndex) =>
+            prevIndex < filteredNotes.length + 1 ? prevIndex + 1 : prevIndex
+          );
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  }, [isOpen, filteredNotes.length]);
+
   const handleKeyPress = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === ">") {
       setShowSubMenu(true);
     } else if (e.key === "Backspace" && searchTerm === ">") {
       setShowSubMenu(false);
+    } else if (e.key === "Enter") {
+      if (showSubMenu && selectedIndex === 0) {
+        createNewNote();
+      } else if (showSubMenu && selectedIndex === 1) {
+        goTosettings();
+      } else if (showSubMenu && selectedIndex === 2) {
+        switchTheme();
+      } else {
+        const selectedNote = filteredNotes[selectedIndex];
+        handleClickNote(selectedNote);
+      }
     }
   };
 
-  // Function to handle clicking a note or submenu option
   const handleClickNote = async (note: Note) => {
     await onClickNote(note);
-    // Hide the prompt after clicking a note
-    isOpen && setIsShortcutPromptOpen(false);
+    isOpen && setIsCommandPromptOpen(false);
   };
 
-  const handleClickSubMenuOption = () => {
-    // Hide the prompt after clicking a submenu option
-    isOpen && setIsShortcutPromptOpen(false);
+  const createNewNote = () => {
+    onCreateNewNote();
+    isOpen && setIsCommandPromptOpen(false);
   };
 
-  // Filter the notes based on the search term
-  const filteredNotes = notes.filter((note) =>
-    note.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const switchTheme = () => {
+    toggleTheme();
+    isOpen && setIsCommandPromptOpen(false);
+  };
+
+  const navigate = useNavigate();
+
+  const goTosettings = () => {
+    navigate("/settings");
+  }
 
   return (
     <>
       {isOpen && (
-        <div className="fixed top-10 left-0 right-0 mx-auto w-1/4 flex flex-col items-center justify-center z-100">
+        <div className="fixed top-10 left-0 right-0 mx-auto sm:w-1/4 w-3/4 flex flex-col items-center justify-center z-100">
           <div className="bg-[#FDFDFA] dark:bg-[#353333] transform w-full rounded-lg shadow-xl py-2 px-4 relative">
-            <div className="w-full p-2 border-b-2 dark:border-neutral-600 bg-transparent align-middle inline text-gray-800 cursor-pointer flex items-center justify-start dark:text-white mr-2">
+            <div className="w-full p-2 border-b-2 dark:border-neutral-600 bg-transparent align-middle text-gray-800 cursor-pointer flex items-center justify-start dark:text-white mr-2">
               <div>
                 <Search2LineIcon className="text-gray-800 dark:text-white h-6 w-6" />
               </div>
               <input
                 className="text-xl text-gray-800 bg-transparent px-2 outline-none dark:text-white w-full"
                 type="text"
-                placeholder="Search notes..."
+                placeholder="Search file or type '>' to search commands"
                 value={searchTerm}
                 onChange={(e) => setSearchTerm(e.target.value)}
-                onKeyDown={handleKeyPress} // Call handleKeyPress function on key down
+                onKeyDown={handleKeyPress}
               />
             </div>
             <div className="py-1">
               {showSubMenu ? (
                 <>
                   <div
-                    className="note-item cursor-pointer hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400 rounded-lg p-2"
-                    onClick={handleClickSubMenuOption}
+                    className={`note-item cursor-pointer rounded-lg p-2 ${
+                      selectedIndex === 0
+                        ? "bg-amber-400 bg-opacity-10 text-amber-400"
+                        : "hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400"
+                    }`}
+                    onClick={createNewNote}
                   >
-                    <h1 className="text-lg">Submenu Item 1</h1>
+                    <h1 className="text-lg">Create new note</h1>
                   </div>
                   <div
-                    className="note-item cursor-pointer hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400 rounded-lg p-2"
-                    onClick={handleClickSubMenuOption}
+                    className={`note-item cursor-pointer rounded-lg p-2 ${
+                      selectedIndex === 1
+                        ? "bg-amber-400 bg-opacity-10 text-amber-400"
+                        : "hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400"
+                    }`}
+                    onClick={goTosettings}
                   >
-                    <h1 className="text-lg">Submenu Item 2</h1>
+                    <h1 className="text-lg">Settings</h1>
+                  </div>
+                  <div
+                    className={`note-item cursor-pointer rounded-lg p-2 ${
+                      selectedIndex === 2
+                        ? "bg-amber-400 bg-opacity-10 text-amber-400"
+                        : "hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400"
+                    }`}
+                    onClick={switchTheme}
+                  >
+                    <h1 className="text-lg">Change Theme</h1>
                   </div>
                 </>
               ) : (
-                filteredNotes.map((note) => (
+                filteredNotes.map((note, index) => (
                   <div
                     key={note.id}
-                    className="note-item cursor-pointer hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400 rounded-lg p-2"
+                    className={`note-item cursor-pointer rounded-lg p-2 ${
+                      index === selectedIndex - 2
+                        ? "bg-amber-400 bg-opacity-10 text-amber-400"
+                        : "hover:bg-amber-400 hover:bg-opacity-10 hover:text-amber-400"
+                    }`}
                     onClick={() => handleClickNote(note)}
                   >
                     <h1 className="text-lg">{note.title}</h1>
@@ -97,4 +160,4 @@ const ShortcutPrompt: React.FC<ShortcutPromptProps> = ({
   );
 };
 
-export default ShortcutPrompt;
+export default CommandPrompt;
