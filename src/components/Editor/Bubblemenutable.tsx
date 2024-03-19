8d00fde729ed8b40e298037022204e2e546dbb18
Daniele Rolli <danielerolli@proton.me>
2024-03-19 09:04:47 +0100

    ☁️ Floating menu for tables

--- src/components/Editor/Bubblemenutable.tsx
+++ src/components/Editor/Bubblemenutable.tsx
@@ -1,28 +1,34 @@
 import React, { useState, useEffect } from "react";
 import { Editor, BubbleMenu } from "@tiptap/react";
-
-//icons
 import InsertRowTopIcon from "remixicon-react/InsertRowTopIcon";
-import InsertRowBottomIcon from "remixicon-react/InsertRowBottomIcon";
 import DeleteRow from "remixicon-react/DeleteRowIcon";
+import DeleteColumn from "remixicon-react/DeleteColumnIcon";
+import InsertRowBottomIcon from "remixicon-react/InsertRowBottomIcon";
 import InsertColumnLeftIcon from "remixicon-react/InsertColumnLeftIcon";
 import InsertColumnRightIcon from "remixicon-react/InsertColumnRightIcon";
-import DeleteColumn from "remixicon-react/DeleteColumnIcon";
-import Brush2Fill from "remixicon-react/Brush2LineIcon";
+import Brush2Fill from "remixicon-react/Brush2FillIcon";
 
 interface Props {
   editor: Editor | null;
 }
 
-const BlubblemenuTable: React.FC<Props> = ({ editor }) => {
-  const [menuOpen, setMenuOpen] = useState(false);
+const BubbleMenuTable: React.FC<Props> = ({ editor }) => {
+  const [menuOpen, setMenuOpen] = useState<boolean>(false);
 
   useEffect(() => {
-    if (!editor) return;
+    const handleMenuState = () => {
+      const isTableCellActive = editor?.isActive("tableCell");
+      const isTableHeaderActive = editor?.isActive("tableHeader");
+      setMenuOpen(!!(isTableCellActive || isTableHeaderActive));
+    };
 
-    const isTableCellActive = editor.isActive("tableCell");
-    const isTableHeaderActive = editor.isActive("tableHeader");
-    setMenuOpen(isTableCellActive || isTableHeaderActive);
+    handleMenuState(); // Initial call to set menu state
+
+    editor?.on("transaction", handleMenuState);
+
+    return () => {
+      editor?.off("transaction", handleMenuState);
+    };
   }, [editor]);
 
   return (
@@ -32,121 +38,122 @@ const BlubblemenuTable: React.FC<Props> = ({ editor }) => {
           editor={editor}
           shouldShow={() => true}
           tippyOptions={{ duration: 100 }}
-          className="bg-[#2D2C2C] p-1 rounded-lg max-w-xs"
         >
-          <div style={{ display: menuOpen ? "block" : "none" }}>
-            <button
+          <div className={menuOpen ? "block" : "hidden"}>
+            <div className="bg-[#2D2C2C] p-1 rounded-lg max-w-xs">
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().addRowAfter().run()}
+          >
+            <InsertRowBottomIcon
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().addRowAfter().run()}
-            >
-              <InsertRowBottomIcon
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
-            <button
+            />
+          </button>
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().addRowBefore().run()}
+          >
+            <InsertRowTopIcon
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().addRowBefore().run()}
-            >
-              <InsertRowTopIcon
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
-            <button
+            />
+          </button>
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().deleteRow().run()}
+          >
+            <DeleteRow
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().deleteRow().run()}
-            >
-              <DeleteRow
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
-            <button
+            />
+          </button>
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().addColumnBefore().run()}
+          >
+            <InsertColumnLeftIcon
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().addColumnBefore().run()}
-            >
-              <InsertColumnLeftIcon
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
-            <button
+            />
+          </button>
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().addColumnAfter().run()}
+          >
+            <InsertColumnRightIcon
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().addColumnAfter().run()}
-            >
-              <InsertColumnRightIcon
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
-            <button
+            />
+          </button>
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().deleteColumn().run()}
+          >
+            <DeleteColumn
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().deleteColumn().run()}
-            >
-              <DeleteColumn
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
-            <button
+            />
+          </button>
+          <button
+            className={
+              editor?.isActive("heading", { level: 1 })
+                ? "p-1 text-amber-400 cursor-pointer"
+                : "p-1 bg-transparent cursor-pointer"
+            }
+            onClick={() => editor?.chain().focus().toggleHeaderCell().run()}
+          >
+            <Brush2Fill
               className={
                 editor?.isActive("heading", { level: 1 })
-                  ? "p-1 text-amber-400 cursor-pointer"
-                  : "p-1 bg-transparent cursor-pointer"
+                  ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
+                  : "p-1 border-none text-white text-xl w-9 h-9"
               }
-              onClick={() => editor?.chain().focus().toggleHeaderCell().run()}
-            >
-              <Brush2Fill
-                className={
-                  editor?.isActive("heading", { level: 1 })
-                    ? "p-1 border-none text-amber-300 text-xl w-9 h-9"
-                    : "p-1 border-none text-white text-xl w-9 h-9"
-                }
-              />
-            </button>
+            />
+          </button>
+          </div>
           </div>
         </BubbleMenu>
       )}
@@ -154,4 +161,4 @@ const BlubblemenuTable: React.FC<Props> = ({ editor }) => {
   );
 };
 
-export default BlubblemenuTable;
+export default BubbleMenuTable;
