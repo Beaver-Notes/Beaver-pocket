bad252bf8de491f1abcdfcef3d1d702eed2412ee
Daniele Rolli <danielerolli@proton.me>
2024-03-21 14:00:53 +0100

    🍦 Expand editor

--- dist/assets/web-lPMVcSl-.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-KBSEh1P6.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ dist/assets/web-lVmE96Cr.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-E0leE74U.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
