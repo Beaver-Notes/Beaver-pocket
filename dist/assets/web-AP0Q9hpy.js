e5e8efcbac3dce2dba68899cbb4629d418a5aea5
Daniele Rolli <danielerolli@proton.me>
2024-04-01 18:52:20 +0200

    🚪Open embedded files

---
+++ dist/assets/web-AP0Q9hpy.js
@@ -0,0 +1,1 @@
+import{W as e}from"./index-i7tTbRLS.js";class i extends e{openFile(o){throw this.unimplemented("Not implemented on web.")}}export{i as FileOpenerWeb};
---
+++ dist/assets/web-LKPQjHR0.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-i7tTbRLS.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
