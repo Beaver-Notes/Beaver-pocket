d9d343a877eaeb9ad10a7d023cc40a2f87ccf403
Daniele Rolli <danielerolli@proton.me>
2024-06-23 20:36:07 +0200

    🎨 Fixing UI

---
+++ dist/assets/clone-nnH-jbqc.js
@@ -0,0 +1,1 @@
+import{c as r}from"./graph-Dqdmp1EZ.js";var e=4;function a(o){return r(o,e)}export{a as c};
--- dist/assets/web-lG_BI8rx.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-xETeexp5.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
