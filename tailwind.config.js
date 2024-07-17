4aaf28d5d5cd287949bdad3d13437812e01a729a
Daniele Rolli <danielerolli@proton.me>
2024-07-17 22:24:31 +0200

    🎨 Better editor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        "t-sm": "0 -1px 2px 0 rgba(0, 0, 0, 0.05)",
+        "t-md":
+          "0 -4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)",
+        "t-lg":
+          "0 -10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)",
+        "t-xl":
+          "0 -20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
+        "t-2xl": "0 -25px 50px -12px rgba(0, 0, 0, 0.25)",
+        "t-3xl": "0 -35px 60px -15px rgba(0, 0, 0, 0.3)",
+      },
+    },
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  plugins: [require("@tailwindcss/typography")],
+};
