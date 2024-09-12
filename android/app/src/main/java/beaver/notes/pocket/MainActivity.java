ac2e6d79796ceff4a53b380322540e0d45c2a8e9
Daniele Rolli <danielerolli@proton.me>
2024-09-12 19:49:58 +0200

    Revert "Delete android directory"
    This reverts commit 519ec420689ccd88e21a16b39a045e38e25d88ab.

---
+++ android/app/src/main/java/beaver/notes/pocket/MainActivity.java
@@ -0,0 +1,16 @@
+package beaver.notes.pocket;
+
+import android.os.Bundle;
+import com.getcapacitor.BridgeActivity;
+import android.content.pm.ActivityInfo;
+
+public class MainActivity extends BridgeActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if(getResources().getBoolean(R.bool.portrait_only)){
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+}
