e7006f00f3ad9e370104a3da00906ce017403fa3
Daniele Rolli <danielerolli@proton.me>
2024-09-12 19:45:51 +0200

    Delete ios directory

--- ios/App/App/MyViewController.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-import Capacitor
-
-class MyViewController: CAPBridgeViewController {
-    override open func capacitorDidLoad() {
-        bridge?.registerPluginInstance(iCloudPlugin())
-    }
-}
