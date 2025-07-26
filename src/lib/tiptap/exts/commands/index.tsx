d366277ec1c9057db2c5baf147ad3962454a9f2c
Daniele Rolli <danielerolli@proton.me>
2025-07-26 13:25:45 +0200

    fix: positioning

--- src/lib/tiptap/exts/commands/index.tsx
+++ src/lib/tiptap/exts/commands/index.tsx
@@ -5,16 +5,24 @@ import { createRoot } from "react-dom/client";
 import { PluginKey } from "prosemirror-state";
 import Commands from "./Commands";
 
-interface EditorSuggestionProps {
+interface CommandsProps {
   translations: any;
   noteId: string;
   handleAddIframe: any;
   handleImageUpload: any;
   handleFileUpload: any;
   handleVideoUpload: any;
+  editor: any;
+  range: any;
+  query: string;
+  text: string;
+  items: any[];
+  command: (props: any) => void;
+  decorationNode: any;
+  clientRect: () => DOMRect;
 }
 
-export default Extension.create<EditorSuggestionProps>({
+export default Extension.create<CommandsProps>({
   name: "slashCommand",
 
   addOptions() {
@@ -26,6 +34,14 @@ export default Extension.create<EditorSuggestionProps>({
       handleFileUpload: () => {},
       handleVideoUpload: () => {},
       handleAddIframe: () => {},
+      editor: null,
+      range: null,
+      query: "",
+      text: "",
+      items: [],
+      command: () => {},
+      decorationNode: null,
+      clientRect: () => new DOMRect(),
       suggestion: {
         char: "/",
         pluginKey: new PluginKey("commands"),
@@ -38,7 +54,7 @@ export default Extension.create<EditorSuggestionProps>({
       Suggestion({
         editor: this.editor,
         pluginKey: new PluginKey("commands"),
-        char: "/", // Listening for the '/' character
+        char: "/",
         command: ({ editor, range, props }) => {
           editor.chain().focus().deleteRange(range).run();
           props.action();
@@ -49,30 +65,65 @@ export default Extension.create<EditorSuggestionProps>({
 
           return {
             onStart: (props: any) => {
+              const wrapper = document.createElement("div");
+              wrapper.style.position = "absolute";
+              wrapper.style.zIndex = "9999";
+
               popup = tippy("body", {
                 getReferenceClientRect: props.clientRect,
                 appendTo: () => document.body,
-                content: () => {
-                  const element = document.createElement("div");
-                  root = createRoot(element);
-
-                  root.render(
-                    <Commands
-                      noteId={this.options.noteId}
-                      editor={this.editor}
-                      query={props.query}
-                      range={props.range}
-                    />
-                  );
-
-                  return element;
-                },
+                content: wrapper,
                 showOnCreate: true,
                 interactive: true,
                 trigger: "manual",
                 placement: "bottom-start",
+                popperOptions: {
+                  strategy: "absolute",
+                  modifiers: [
+                    {
+                      name: "flip",
+                      options: {
+                        boundary: document.body,
+                        fallbackPlacements: [
+                          "top-start",
+                          "bottom-end",
+                          "top-end",
+                        ],
+                      },
+                    },
+                    {
+                      name: "preventOverflow",
+                      options: {
+                        boundary: document.body,
+                        tether: false,
+                        altAxis: true,
+                      },
+                    },
+                    {
+                      name: "offset",
+                      options: {
+                        offset: [0, 8],
+                      },
+                    },
+                  ],
+                },
+                hideOnClick: false,
+                arrow: false,
+                theme: "light",
+                maxWidth: "none",
               });
+
+              root = createRoot(wrapper);
+              root.render(
+                <Commands
+                  noteId={this.options.noteId}
+                  editor={this.editor}
+                  query={props.query}
+                  range={props.range}
+                />
+              );
             },
+
             onUpdate: (props: any) => {
               if (root) {
                 root.render(
@@ -84,12 +135,21 @@ export default Extension.create<EditorSuggestionProps>({
                   />
                 );
               }
+
+              if (popup && popup[0] && props.clientRect) {
+                popup[0].setProps({
+                  getReferenceClientRect: props.clientRect,
+                });
+              }
             },
+
             onExit: () => {
               if (root) {
                 root.unmount();
               }
-              popup[0].destroy();
+              if (popup && popup[0]) {
+                popup[0].destroy();
+              }
             },
           };
         },
--- src/lib/tiptap/exts/suggestion/index.ts
+++ src/lib/tiptap/exts/suggestion/index.ts
@@ -1,9 +1,9 @@
-import { Node, mergeAttributes } from '@tiptap/core';
-import { PluginKey } from 'prosemirror-state';
-import Suggestion from '@tiptap/suggestion';
-import { ReactRenderer } from '@tiptap/react';
-import tippy, { Instance as TippyInstance } from 'tippy.js';
-import SuggestionComponent from './SuggestionComponent';
+import { Node, mergeAttributes } from "@tiptap/core";
+import { PluginKey } from "prosemirror-state";
+import Suggestion from "@tiptap/suggestion";
+import { ReactRenderer } from "@tiptap/react";
+import tippy, { Instance as TippyInstance } from "tippy.js";
+import SuggestionComponent from "./SuggestionComponent";
 
 interface MentionNodeOptions {
   name: string;
@@ -22,10 +22,10 @@ interface SuggestionProps {
   clientRect: () => DOMRect;
 }
 
-export default function createMentionNode({ 
-  name, 
-  props: customProps = {}, 
-  configure = {} 
+export default function createMentionNode({
+  name,
+  props: customProps = {},
+  configure = {},
 }: MentionNodeOptions) {
   return Node.create({
     name,
@@ -39,7 +39,7 @@ export default function createMentionNode({
           }`;
         },
         suggestion: {
-          char: '@',
+          char: "@",
           pluginKey: new PluginKey(name),
           render: () => {
             let component: ReactRenderer;
@@ -52,46 +52,114 @@ export default function createMentionNode({
                   editor: props.editor,
                 });
 
-                popup = tippy('body', {
+                popup = tippy("body", {
                   getReferenceClientRect: props.clientRect,
                   appendTo: () => document.body,
                   content: component.element,
                   showOnCreate: true,
                   interactive: true,
-                  trigger: 'manual',
-                  placement: 'bottom-start',
+                  trigger: "manual",
+                  placement: "bottom-start",
+                  popperOptions: {
+                    strategy: "absolute",
+                    modifiers: [
+                      {
+                        name: "flip",
+                        options: {
+                          boundary: document.body,
+                          fallbackPlacements: [
+                            "top-start",
+                            "bottom-end",
+                            "top-end",
+                            "bottom-start",
+                            "right-start",
+                            "left-start",
+                          ],
+                          padding: 8,
+                        },
+                      },
+                      {
+                        name: "preventOverflow",
+                        options: {
+                          boundary: document.body,
+                          tether: false,
+                          altAxis: true,
+                          padding: 8,
+                        },
+                      },
+                      {
+                        name: "offset",
+                        options: {
+                          offset: [0, 8],
+                        },
+                      },
+                      {
+                        name: "computeStyles",
+                        options: {
+                          adaptive: true,
+                          roundOffsets: true,
+                        },
+                      },
+                    ],
+                  },
+                  hideOnClick: false,
+                  arrow: false,
+                  theme: "light-border",
+                  maxWidth: 320,
+                  zIndex: 9999,
+                  animation: "shift-away-subtle",
+                  duration: [200, 150],
                 });
               },
-              onUpdate(props: SuggestionProps) {
-                component.updateProps({ ...props, ...customProps });
 
-                popup[0].setProps({
-                  getReferenceClientRect: props.clientRect,
-                });
+              onUpdate(props: SuggestionProps) {
+                if (component) {
+                  component.updateProps({ ...props, ...customProps });
+                }
+
+                if (popup && popup[0] && props.clientRect) {
+                  popup[0].setProps({
+                    getReferenceClientRect: props.clientRect,
+                  });
+                }
               },
+
               onKeyDown(props: { event: KeyboardEvent }) {
-                if (props.event.key === 'Escape') {
-                  popup[0].hide();
+                if (props.event.key === "Escape") {
+                  if (popup && popup[0]) {
+                    popup[0].hide();
+                  }
                   return true;
                 }
 
-                return (component.ref as { onKeyDown?: (props: any) => boolean })?.onKeyDown?.(props);
+                return (
+                  (
+                    component.ref as { onKeyDown?: (props: any) => boolean }
+                  )?.onKeyDown?.(props) ?? false
+                );
               },
+
               onExit() {
-                popup[0].destroy();
-                component.destroy();
+                if (popup && popup[0]) {
+                  popup[0].destroy();
+                }
+                if (component) {
+                  component.destroy();
+                }
               },
             };
           },
-          command: ({ editor, range, props }: { 
-            editor: any; 
-            range: any; 
-            props: any; 
+          command: ({
+            editor,
+            range,
+            props,
+          }: {
+            editor: any;
+            range: any;
+            props: any;
           }) => {
-            // increase range.to by one when the next node is of type "text"
-            // and starts with a space character
             const nodeAfter = editor.view.state.selection.$to.nodeAfter;
-            const overrideSpace = nodeAfter?.text?.startsWith(' ');
+            const overrideSpace = nodeAfter?.text?.startsWith(" ");
 
             if (overrideSpace) {
               range.to += 1;
@@ -106,8 +174,8 @@ export default function createMentionNode({
                   attrs: props,
                 },
                 {
-                  type: 'text',
-                  text: ' ',
+                  type: "text",
+                  text: " ",
                 },
               ])
               .run();
@@ -119,7 +187,7 @@ export default function createMentionNode({
       };
     },
 
-    group: 'inline',
+    group: "inline",
 
     inline: true,
 
@@ -137,9 +205,9 @@ export default function createMentionNode({
 
     renderHTML({ node, HTMLAttributes }: { node: any; HTMLAttributes: any }) {
       return [
-        'span',
+        "span",
         mergeAttributes(
-          { [`data-mention`]: '' },
+          { [`data-mention`]: "" },
           this.options.HTMLAttributes,
           HTMLAttributes
         ),
@@ -156,7 +224,7 @@ export default function createMentionNode({
           default: null,
           parseHTML: (element: HTMLElement) => {
             return {
-              id: element.getAttribute('data-id'),
+              id: element.getAttribute("data-id"),
             };
           },
           renderHTML: (attributes: any) => {
@@ -165,7 +233,7 @@ export default function createMentionNode({
             }
 
             return {
-              'data-id': attributes.id,
+              "data-id": attributes.id,
             };
           },
         },
@@ -174,7 +242,7 @@ export default function createMentionNode({
           default: null,
           parseHTML: (element: HTMLElement) => {
             return {
-              label: element.getAttribute('data-label'),
+              label: element.getAttribute("data-label"),
             };
           },
           renderHTML: (attributes: any) => {
@@ -183,7 +251,7 @@ export default function createMentionNode({
             }
 
             return {
-              'data-label': attributes.label,
+              "data-label": attributes.label,
             };
           },
         },
@@ -200,30 +268,36 @@ export default function createMentionNode({
     addKeyboardShortcuts() {
       return {
         Backspace: () =>
-          this.editor.commands.command(({ tr, state }: { tr: any; state: any }) => {
-            let isMention = false;
-            const { selection } = state;
-            const { empty, anchor } = selection;
+          this.editor.commands.command(
+            ({ tr, state }: { tr: any; state: any }) => {
+              let isMention = false;
+              const { selection } = state;
+              const { empty, anchor } = selection;
 
-            if (!empty) {
-              return false;
-            }
-
-            state.doc.nodesBetween(anchor - 1, anchor, (node: any, pos: number) => {
-              if (node.type.name === this.name) {
-                isMention = true;
-                tr.insertText(
-                  this.options.suggestion.char || '',
-                  pos,
-                  pos + node.nodeSize
-                );
-
+              if (!empty) {
                 return false;
               }
-            });
 
-            return isMention;
-          }),
+              state.doc.nodesBetween(
+                anchor - 1,
+                anchor,
+                (node: any, pos: number) => {
+                  if (node.type.name === this.name) {
+                    isMention = true;
+                    tr.insertText(
+                      this.options.suggestion.char || "",
+                      pos,
+                      pos + node.nodeSize
+                    );
+
+                    return false;
+                  }
+                }
+              );
+
+              return isMention;
+            }
+          ),
       };
     },
 
